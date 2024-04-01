ee0be2a2caf8298b1b61f90aca8b142f9a0e484f
ds-exe <daniel@ds-exe.dev>
2024-04-01 16:29:25 +0100

    Implement now command

--- Modules/TimeModule.cs
+++ Modules/TimeModule.cs
@@ -3,17 +3,24 @@
     internal class TimeModule : BaseCommandModule
     {
         [Command("time")]
-        public async Task Time(CommandContext ctx)
+        public async Task Time(CommandContext ctx, params string[] queryList)
         {
             // need to process timezone with paramter
-            ctx.RespondAsync(DateTime.UtcNow.Timestamp(TimestampFormat.LongDateTime));
+            await ctx.RespondAsync(DateTime.UtcNow.Timestamp(TimestampFormat.LongDateTime));
         }
 
         [Command("until")]
-        public async Task Until(CommandContext ctx)
+        public async Task Until(CommandContext ctx, params string[] queryList)
         {
             // need to process timezone with paramter
-            ctx.RespondAsync(DateTime.UtcNow.Timestamp());
+            await ctx.RespondAsync(DateTime.UtcNow.Timestamp());
+        }
+
+        [Command("now")]
+        public async Task Now(CommandContext ctx, string timezone = "utc")
+        {
+            var zone = TimeZoneInfo.GetSystemTimeZones().First(t => t.DisplayName.ToLower().Contains(timezone.ToLower()));
+            await ctx.RespondAsync($"`{TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, zone).ToString("dd MMM yyyy, HH:mm")}`");
         }
 
         public static string GenerateUnixTimeNow()
