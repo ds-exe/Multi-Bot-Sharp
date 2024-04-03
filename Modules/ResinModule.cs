5066cf0cb814f6a430c6edd9741ded521416bec7
ds-exe <daniel@ds-exe.dev>
2024-04-03 01:04:01 +0100

    Rework base logic for resin module

--- Modules/ResinModule.cs
+++ Modules/ResinModule.cs
@@ -9,71 +9,41 @@
         };
 
         [Command("resin")]
-        public async Task Resin(CommandContext ctx, params string[] queryArray)
+        public async Task Resin(CommandContext ctx, [Description("hsr or genshin")] string game, [Description("positive value to set, negative value to reduce")] int? resin = null)
         {
-            if (queryArray.Length == 0)
-            {
-                await ctx.RespondAsync("No parameter given");
-                return;
-            }
-
-            if (queryArray[0].ToLower() == "help")
-            {
-                await ctx.RespondAsync("Help message for resin");
-                return;
-            }
-
-            ResinData resinData = GetResinData(queryArray);
-
-            if (resinData.Game == null || resinData.Account == null)
+            if (!IsValidGame(game))
             {
                 await ctx.RespondAsync("Resin error message");
                 return;
             }
 
-            // need to continue processing
-            await ctx.RespondAsync("Pass");
+            if (resin == null)
+            {
+                await ctx.RespondAsync("Print current games resin embed");
+                return;
+            }
+
+            await ctx.RespondAsync("TODO");
         }
 
-        public ResinData GetResinData(string[] queryArray)
+        public bool IsValidGame(string game)
         {
-            ResinData data = new ResinData();
-
-            var matches = Regex.Match(queryArray[0],@"^([A-z]+)[0-9]?$");
-
-            if (!matches.Success)
-            {
-                return data;
-            }
-
-            var account = matches.Groups[0].Value;
-            var game = matches.Groups[1].Value;
-
-            if (!games.ContainsKey(game))
-            {
-                return data;
-            }
-
-            data.Account = account;
-            data.Game = game;
-
-            // Need to grab resin data
-            return data;
+            return games.ContainsKey(game);
         }
-
-
 
         [Command("hsr")]
-        public async Task Hsr(CommandContext ctx, params string[] queryArray)
+        [Description("Shortcut for resin hsr")]
+        public async Task Hsr(CommandContext ctx, [Description("positive value to set, negative value to reduce")] int? resin = null)
         {
-            await Resin(ctx, queryArray.Prepend("hsr").ToArray());
+            await Resin(ctx, "hsr", resin);
             return;
         }
 
         [Command("genshin")]
-        public async Task Genshin(CommandContext ctx, params string[] queryArray)
+        [Description("Shortcut for resin genshin")]
+        public async Task Genshin(CommandContext ctx, [Description("positive value to set, negative value to reduce")] int? resin = null)
         {
-            await Resin(ctx, queryArray.Prepend("genshin").ToArray());
+            await Resin(ctx, "genshin", resin);
             return;
         }
     }
