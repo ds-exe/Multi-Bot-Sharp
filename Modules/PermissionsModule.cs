36322834bcdbbd11200539e45b094bd3ce9b8ad1
ds-exe <daniel@ds-exe.dev>
2024-04-06 18:37:59 +0100

    Add Perms grouping

---
+++ Modules/PermissionsModule.cs
@@ -0,0 +1,26 @@
+﻿namespace Multi_Bot_Sharp.Modules
+{
+    [Group("perms")]
+    [Description("Commands to enable permission for bot usage")]
+    public class PermissionsModule : BaseCommandModule
+    {
+        [GroupCommand, Command("help")]
+        [Description("Lists sub commands")]
+        public async Task Help(CommandContext ctx)
+        {
+
+        }
+
+        [Command("listusers")]
+        public async Task ListUsers(CommandContext ctx)
+        {
+            await ctx.RespondAsync("List Users");
+        }
+
+        [Command("listroles")]
+        public async Task ListRoles(CommandContext ctx)
+        {
+            await ctx.RespondAsync("List Roles");
+        }
+    }
+}
