6d20f3f8ea0cf7253590e3a68d8497231054cba1
ds-exe <daniel@ds-exe.dev>
2026-01-18 02:17:14 +0000

    Update dependencies, Code Tidy

--- Commands/HelpCommandModule.cs
+++ Commands/HelpCommandModule.cs
@@ -4,16 +4,16 @@ namespace Multi_Bot_Sharp.Commands;
 
 public class HelpCommandModule : ApplicationCommandsModule
 {
-    private const ulong creatorID = 74968333413257216;
-    private DiscordUser User;
-    private string EmbedThumbnail;
+    private const ulong CreatorId = 74968333413257216;
+    private readonly DiscordUser _user;
+    private readonly string _embedThumbnail;
 
     public HelpCommandModule(DiscordClient discordClient)
     {
         var config = ConfigHelper.GetJsonObject<Config>("config");
-        var botOwner = config.Owner == 0 ? creatorID : config.Owner;
-        User = discordClient.GetUserAsync(botOwner).Result;
-        EmbedThumbnail = config.EmbedThumbnail ?? "";
+        var botOwner = config.Owner == 0 ? CreatorId : config.Owner;
+        _user = discordClient.GetUserAsync(botOwner).Result;
+        _embedThumbnail = config.EmbedThumbnail ?? "";
     }
 
     // Initial code based on DisCatSharp default help command
@@ -22,9 +22,9 @@ public class HelpCommandModule : ApplicationCommandsModule
         InteractionContext ctx,
         [Autocomplete(typeof(DefaultHelpAutoCompleteProvider)), Option("option_one", "top level command to provide help for", true)] string commandName)
     {
-        List<DiscordApplicationCommand> applicationCommands = null;
+        List<DiscordApplicationCommand> applicationCommands;
         var globalCommandsTask = ctx.Client.GetGlobalApplicationCommandsAsync();
-        if (ctx.Guild != null)
+        if (ctx.Guild is not null)
         {
             var guildCommandsTask = ctx.Client.GetGuildApplicationCommandsAsync(ctx.Guild.Id);
             await Task.WhenAll(globalCommandsTask, guildCommandsTask).ConfigureAwait(false);
@@ -53,7 +53,7 @@ public class HelpCommandModule : ApplicationCommandsModule
         if (command is null)
         {
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                    .WithContent($"No command called {commandName} in guild {ctx.Guild.Name}").AsEphemeral()).ConfigureAwait(false);
+                    .WithContent($"No command called {commandName} in guild {ctx.Guild?.Name}").AsEphemeral()).ConfigureAwait(false);
             return;
         }
 
@@ -61,11 +61,11 @@ public class HelpCommandModule : ApplicationCommandsModule
         {
             Title = "Help",
             Description = $"{command.Mention}: {command.Description ?? "No description provided."}"
-        }.AddField(new("Command is NSFW", command.IsNsfw.ToString()));
+        }.AddField(new DiscordEmbedField("Command is NSFW", command.IsNsfw.ToString()));
         // Custom help start
         discordEmbed.Color = new DiscordColor("0099ff");
-        discordEmbed.WithThumbnail(EmbedThumbnail);
-        discordEmbed.WithFooter($"BOT owner @{User.Username}", User.AvatarUrl);
+        discordEmbed.WithThumbnail(_embedThumbnail);
+        discordEmbed.WithFooter($"BOT owner @{_user.Username}", _user.AvatarUrl);
         // Custom help end
         if (command.Options is not null)
         {
@@ -76,27 +76,27 @@ public class HelpCommandModule : ApplicationCommandsModule
                 sb.Append('`').Append(option.Name).Append("`: ").Append(option.Description ?? "No description provided.").Append('\n');
 
             sb.Append('\n');
-            discordEmbed.AddField(new("Arguments", sb.ToString().Trim()));
+            discordEmbed.AddField(new DiscordEmbedField("Arguments", sb.ToString().Trim()));
         }
 
         await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
                 new DiscordInteractionResponseBuilder().AddEmbed(discordEmbed).AsEphemeral()).ConfigureAwait(false);
     }
 
-    public sealed class DefaultHelpAutoCompleteProvider : IAutocompleteProvider
+    private sealed class DefaultHelpAutoCompleteProvider : IAutocompleteProvider
     {
         public async Task<IEnumerable<DiscordApplicationCommandAutocompleteChoice>> Provider(AutocompleteContext context)
         {
-            IEnumerable<DiscordApplicationCommand> slashCommands = null;
+            IEnumerable<DiscordApplicationCommand> slashCommands;
             var globalCommandsTask = context.Client.GetGlobalApplicationCommandsAsync();
-            if (context.Guild != null)
+            if (context.Guild is not null)
             {
                 var guildCommandsTask = context.Client.GetGuildApplicationCommandsAsync(context.Guild.Id);
                 await Task.WhenAll(globalCommandsTask, guildCommandsTask).ConfigureAwait(false);
                 slashCommands = globalCommandsTask.Result.Concat(guildCommandsTask.Result)
                     .Where(ac => !ac.Name.Equals("help", StringComparison.OrdinalIgnoreCase))
                     .GroupBy(ac => ac.Name).Select(x => x.First())
-                    .Where(ac => ac.Name.StartsWith(context.Options[0].Value.ToString(), StringComparison.OrdinalIgnoreCase))
+                    .Where(ac => ac.Name.StartsWith(context.Options[0].Value.ToString() ?? string.Empty, StringComparison.OrdinalIgnoreCase))
                     .ToList();
             }
             else
@@ -105,7 +105,7 @@ public class HelpCommandModule : ApplicationCommandsModule
                 slashCommands = globalCommandsTask.Result
                     .Where(ac => !ac.Name.Equals("help", StringComparison.OrdinalIgnoreCase))
                     .GroupBy(ac => ac.Name).Select(x => x.First())
-                    .Where(ac => ac.Name.StartsWith(context.Options[0].Value.ToString(), StringComparison.OrdinalIgnoreCase))
+                    .Where(ac => ac.Name.StartsWith(context.Options[0].Value.ToString() ?? string.Empty, StringComparison.OrdinalIgnoreCase))
                     .ToList();
             }
 
